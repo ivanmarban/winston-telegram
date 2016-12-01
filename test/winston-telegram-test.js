aeab8303a440d9942d68478b76a4f0fd0d32de9a
Ivan Marban <ivan.marban@gmail.com>
2016-12-01 15:42:17 +0100

    Coding style: two spaces for indenting

--- test/winston-telegram-test.js
+++ test/winston-telegram-test.js
@@ -4,34 +4,34 @@
  * (C) 2015 Ivan Marban
  * MIT LICENSE
  */
-var	vows = require('vows'),
-	assert = require('assert'),
-	winston = require('winston'),
-	helpers = require('winston/test/helpers'),
-	Telegram = require('../lib/winston-telegram').Telegram;
+var vows = require('vows'),
+  assert = require('assert'),
+  winston = require('winston'),
+  helpers = require('winston/test/helpers'),
+  Telegram = require('../lib/winston-telegram').Telegram;
 
 var TelegramTransport;
 
-TelegramTransport = new (Telegram)({
-	token : '177492804:AAG318J_PjC03-okUmqQV652EDbf_Rr0vTo',
-	chatid : '-50115750'
+TelegramTransport = new(Telegram)({
+  token: '177492804:AAG318J_PjC03-okUmqQV652EDbf_Rr0vTo',
+  chatid: '-50115750'
 });
 
 function assertTelegram(transport) {
-	assert.instanceOf(transport, Telegram);
-	assert.isFunction(transport.log);
+  assert.instanceOf(transport, Telegram);
+  assert.isFunction(transport.log);
 }
 
 vows.describe('winston-telegram').addBatch({
-	'An instance of the Telegram Transport': {
-		'when passed an options': {
-			'should have the proper methods defined': function () {
-				assertTelegram(TelegramTransport);
-			},
-			'the log() method': helpers.testNpmLevels(TelegramTransport, 'should log messages to Telegram', function (ign, err, logged) {
-				assert.isNull(err);
-				assert.isTrue(logged);
-			})
-		}
-	}
+  'An instance of the Telegram Transport': {
+    'when passed an options': {
+      'should have the proper methods defined': function() {
+        assertTelegram(TelegramTransport);
+      },
+      'the log() method': helpers.testNpmLevels(TelegramTransport, 'should log messages to Telegram', function(ign, err, logged) {
+        assert.isNull(err);
+        assert.isTrue(logged);
+      })
+    }
+  }
 }).export(module);
