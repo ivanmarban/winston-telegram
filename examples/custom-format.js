954d4fb1fd397589c904ce7441a0750390899ed8
Ivan Marban <ivan.marban@gmail.com>
2019-04-28 12:37:33 +0200

    Added examples

---
+++ examples/custom-format.js
@@ -0,0 +1,26 @@
+/*
+ * Using custom format message.
+ */
+
+const logger = require('winston')
+const TelegramLogger = require('winston-telegram')
+
+logger.add(
+  new TelegramLogger({
+    token: 'TELEGRAM_TOKEN',
+    chatId: 'CHAT_ID',
+    level: 'warn',
+    unique: true,
+    formatMessage: function (options) {
+      let message = options.message
+      if (options.level === 'warn') {
+        message = '[Warning] ' + message
+      }
+      return message
+    }
+  })
+)
+
+logger.warn('Some warning!!')
+
+// Output: [Warning] Some warning!!
